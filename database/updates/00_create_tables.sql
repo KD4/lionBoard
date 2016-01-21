a07e1664809b441156b8a32850e3b35ab0859381
KD4 <kangddanddan@gmail.com>
2016-01-21 18:28:34 +0900

    update database infomation.

--- database/updates/00_create_tables.sql
+++ database/updates/00_create_tables.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.6.25)
 # Database: lionboard
-# Generation Time: 2016-01-20 12:45:35 +0000
+# Generation Time: 2016-01-21 09:22:03 +0000
 # ************************************************************
 
 
@@ -31,10 +31,10 @@ CREATE TABLE `CMT_REPORT_TB` (
   `reporterId` int(10) unsigned NOT NULL,
   `reason` varchar(255) NOT NULL DEFAULT '',
   `reportedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `processStatus` int(10) unsigned DEFAULT '4',
+  `processStatus` int(10) unsigned NOT NULL DEFAULT '4',
   PRIMARY KEY (`id`),
   KEY `cmtId` (`cmtId`),
-  CONSTRAINT `cmt_report_tb_ibfk_1` FOREIGN KEY (`cmtId`) REFERENCES `cmt_tb` (`cmtId`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `cmt_report_tb_ibfk_2` FOREIGN KEY (`cmtId`) REFERENCES `CMT_TB` (`cmtId`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -46,10 +46,9 @@ DROP TABLE IF EXISTS `CMT_STATUS_TB`;
 
 CREATE TABLE `CMT_STATUS_TB` (
   `cmtId` int(10) unsigned NOT NULL,
-  `cmtStatus` int(10) unsigned NOT NULL,
-  `pastDays` int(10) unsigned NOT NULL,
+  `cmtStatus` varchar(2) NOT NULL DEFAULT 'S',
+  `pastDays` int(10) unsigned NOT NULL DEFAULT '0',
   KEY `cmtId` (`cmtId`),
-  KEY `stateCode` (`cmtStatus`),
   CONSTRAINT `cmt_status_tb_ibfk_2` FOREIGN KEY (`cmtId`) REFERENCES `CMT_TB` (`cmtId`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -64,11 +63,10 @@ CREATE TABLE `CMT_TB` (
   `cmtId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `postId` int(10) unsigned NOT NULL,
   `userId` int(10) unsigned NOT NULL,
-  `cmtNum` int(10) unsigned NOT NULL,
+  `cmtNum` int(10) unsigned NOT NULL DEFAULT '0',
   `depth` int(11) NOT NULL DEFAULT '0',
-  `contents` text,
+  `contents` text NOT NULL,
   `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `cmtStatus` varchar(2) NOT NULL DEFAULT 'S',
   `likeCount` int(10) unsigned NOT NULL DEFAULT '0',
   `hateCount` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`cmtId`),
@@ -76,6 +74,107 @@ CREATE TABLE `CMT_TB` (
   CONSTRAINT `cmt_tb_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `POST_TB` (`postId`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+DELIMITER ;;
+/*!50003 SET SESSION SQL_MODE="STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION" */;;
+/*!50003 CREATE */ /*!50017 DEFINER=`root`@`localhost` */ /*!50003 TRIGGER `set_cmtNum` BEFORE INSERT ON `CMT_TB` FOR EACH ROW IF NEW.depth < 1 THEN
+  SET @cmtNum = (SELECT cmtNum FROM CMT_TB WHERE depth = 0 AND postId = NEW.postId ORDER BY cmtNum DESC LIMIT 1);
+    IF @cmtNum IS NULL THEN
+      SET NEW.cmtNum = 1000;
+    ELSE
+      SET NEW.cmtNum = @cmtNum + 1000;
+    END IF;
+ELSE
+  SET NEW.cmtNum = NEW.cmtNum - 1;
+END IF */;;
+DELIMITER ;
+/*!50003 SET SESSION SQL_MODE=@OLD_SQL_MODE */;
+
+
+# Dump of table POST_FILE_TB
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `POST_FILE_TB`;
+
+CREATE TABLE `POST_FILE_TB` (
+  `postId` int(10) unsigned NOT NULL,
+  `fileId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fileName` varchar(128) NOT NULL,
+  `fileUrl` varchar(255) NOT NULL,
+  PRIMARY KEY (`fileId`),
+  KEY `postId` (`postId`),
+  CONSTRAINT `post_file_tb_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `POST_TB` (`postId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table POST_REPORT_TB
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `POST_REPORT_TB`;
+
+CREATE TABLE `POST_REPORT_TB` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `postId` int(10) unsigned NOT NULL,
+  `reporterId` int(10) unsigned NOT NULL,
+  `reason` varchar(255) NOT NULL DEFAULT '',
+  `reportedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `processStatus` varchar(2) NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`id`),
+  KEY `postId` (`postId`),
+  CONSTRAINT `post_report_tb_ibfk_3` FOREIGN KEY (`postId`) REFERENCES `POST_TB` (`postId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table POST_STATUS_TB
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `POST_STATUS_TB`;
+
+CREATE TABLE `POST_STATUS_TB` (
+  `postId` int(10) unsigned NOT NULL,
+  `postStatus` varchar(2) NOT NULL DEFAULT 'S',
+  `pastDays` int(10) unsigned NOT NULL DEFAULT '0',
+  KEY `postId` (`postId`),
+  CONSTRAINT `post_status_tb_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `POST_TB` (`postId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+# Dump of table POST_TB
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `POST_TB`;
+
+CREATE TABLE `POST_TB` (
+  `postId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userId` int(10) unsigned NOT NULL,
+  `existFiles` varchar(2) NOT NULL DEFAULT 'F',
+  `postNum` int(10) unsigned NOT NULL DEFAULT '0',
+  `depth` int(10) NOT NULL DEFAULT '0',
+  `title` varchar(128) NOT NULL,
+  `contents` text NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cmtCount` int(10) unsigned NOT NULL DEFAULT '0',
+  `likeCount` int(10) unsigned NOT NULL DEFAULT '0',
+  `hateCount` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`postId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DELIMITER ;;
+/*!50003 SET SESSION SQL_MODE="STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION" */;;
+/*!50003 CREATE */ /*!50017 DEFINER=`root`@`localhost` */ /*!50003 TRIGGER `set_postNum` BEFORE INSERT ON `POST_TB` FOR EACH ROW IF NEW.depth < 1 THEN
+  SET @postNum = (SELECT postNum FROM POST_TB WHERE depth = 0 ORDER BY postNum DESC LIMIT 1);
+    IF @postNum IS NULL THEN
+      SET NEW.postNum = 1000;
+    ELSE
+      SET NEW.postNum = @postNum + 1000;
+    END IF;
+ELSE
+  SET NEW.postNum = NEW.postNum - 1;
+END IF */;;
+DELIMITER ;
+/*!50003 SET SESSION SQL_MODE=@OLD_SQL_MODE */;
 
 
 # Dump of table STICKY_CMT_TB
@@ -107,81 +206,6 @@ CREATE TABLE `STICKY_POST_TB` (
 
 
 
-# Dump of table POST_FILE_TB
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `POST_FILE_TB`;
-
-CREATE TABLE `POST_FILE_TB` (
-  `postId` int(10) unsigned NOT NULL,
-  `fileId` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `fileName` varchar(128) NOT NULL,
-  `fileUrl` varchar(255) NOT NULL,
-  PRIMARY KEY (`fileId`),
-  KEY `postId` (`postId`),
-  CONSTRAINT `post_file_tb_ibfk_1` FOREIGN KEY (`postId`) REFERENCES `post_tb` (`postId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table POST_REPORT_TB
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `POST_REPORT_TB`;
-
-CREATE TABLE `POST_REPORT_TB` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `postId` int(10) unsigned NOT NULL,
-  `reporterId` int(10) unsigned NOT NULL,
-  `reason` varchar(255) NOT NULL DEFAULT '',
-  `reportedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `processStatus` varchar(2) NOT NULL DEFAULT 'N',
-  PRIMARY KEY (`id`),
-  KEY `postId` (`postId`),
-  CONSTRAINT `post_report_tb_ibfk_3` FOREIGN KEY (`postId`) REFERENCES `POST_TB` (`postId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table POST_STATUS_TB
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `POST_STATUS_TB`;
-
-CREATE TABLE `POST_STATUS_TB` (
-  `postId` int(10) unsigned NOT NULL,
-  `postStatus` int(10) unsigned NOT NULL,
-  `pastDays` int(10) unsigned NOT NULL,
-  KEY `postId` (`postId`),
-  KEY `stateCode` (`postStatus`),
-  CONSTRAINT `post_status_tb_ibfk_1` FOREIGN KEY (`postId`) REFERENCES `post_tb` (`postId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Dump of table POST_TB
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `POST_TB`;
-
-CREATE TABLE `POST_TB` (
-  `postId` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `userId` int(10) unsigned NOT NULL,
-  `existFiles` tinyint(1) NOT NULL DEFAULT '0',
-  `postNum` int(10) unsigned NOT NULL,
-  `depth` int(10) NOT NULL DEFAULT '0',
-  `title` varchar(128) NOT NULL,
-  `contents` text,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `postStatus` varchar(1) NOT NULL DEFAULT 'S',
-  `cmtCount` int(10) unsigned NOT NULL DEFAULT '0',
-  `likeCount` int(10) unsigned NOT NULL DEFAULT '0',
-  `hateCount` int(10) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`postId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
 # Dump of table USER_TB
 # ------------------------------------------------------------
 
@@ -196,7 +220,8 @@ CREATE TABLE `USER_TB` (
   `email` varchar(255) NOT NULL DEFAULT '',
   `profileUrl` varchar(255) NOT NULL DEFAULT ' ',
   `userStatus` varchar(2) NOT NULL DEFAULT 'S',
-  `registedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `registeredAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `password` varchar(100) NOT NULL DEFAULT ' ',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 

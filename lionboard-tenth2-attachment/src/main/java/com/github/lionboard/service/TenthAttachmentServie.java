e1665024c70b5febc9490f6b76070eb51a57441e
KD4 <kangddanddan@gmail.com>
2016-02-07 18:00:45 +0900

    how fix : edit logic
    -redirect post page before edit post.

--- lionboard-tenth2-attachment/src/main/java/com/github/lionboard/service/TenthAttachmentServie.java
+++ lionboard-tenth2-attachment/src/main/java/com/github/lionboard/service/TenthAttachmentServie.java
@@ -10,31 +10,28 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TenthAttachmentServie implements AttachmentService{
+
+    @Autowired
+    ImageFileUploadForTenth2 imageFileUploadForTenth2;
+
+
     @Override
     public String uploadFile(byte[] bytes, String fileName) throws Exception {
-        return null;
+        //tenth2에 등록 성공하면, 접근할 수 있는 URL을 반환함.
+        //todo fileName encoding.
+        String uploadUrl = insertFileOnTenthServer(bytes, fileName);
+
+        return uploadUrl;
     }
-//
-//    @Autowired
-//    ImageFileUploadForTenth2 imageFileUploadForTenth2;
-//
-//
-//    @Override
-//    public String uploadFile(byte[] bytes, String fileName) throws Exception {
-//        //tenth2에 등록 성공하면, 접근할 수 있는 URL을 반환함.
-//        String uploadUrl = insertFileOnTenthServer(bytes, fileName);
-//
-//        return uploadUrl;
-//    }
-//
-//    //업로드 팜을 이용해서 tenth서버에 업로드된 파일을 다시 업로드하는 로직.
-//    private String insertFileOnTenthServer(byte[] imageFileBytes, String fileName) throws Exception {
-//        try {
-//            imageFileUploadForTenth2.init();
-//            return imageFileUploadForTenth2.create(imageFileBytes, fileName);
-//        } catch (Exception e) {
-//            throw new Exception(e);
-//        }
-//    }
+
+    //업로드 팜을 이용해서 tenth서버에 업로드된 파일을 다시 업로드하는 로직.
+    private String insertFileOnTenthServer(byte[] imageFileBytes, String fileName) throws Exception {
+        try {
+            imageFileUploadForTenth2.init();
+            return imageFileUploadForTenth2.create(imageFileBytes, fileName);
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
 
 }

842f3e85b001cb1bbd31530f748dc790b92fb364
KD4 <kangddanddan@gmail.com>
2016-02-09 17:25:55 +0900

    Update unshaped feature to sign in with twitter.
    - This feature will be implemented. after the function of cache will be implemented.

--- lionboard-web/src/main/java/com/github/lionboard/controller/SocialController.java
+++ lionboard-web/src/main/java/com/github/lionboard/controller/SocialController.java
@@ -1,7 +1,10 @@
 package com.github.lionboard.controller;
 
+import com.github.lionboard.error.InvalidCmtException;
+import com.github.lionboard.error.InvalidUserException;
 import com.github.lionboard.model.User;
 import com.github.lionboard.service.LionBoardService;
+import common.OAuthHeaderTwitter;
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -81,7 +84,6 @@ public class SocialController {
         InputStreamReader isr = new InputStreamReader(ist);
         BufferedReader br = new BufferedReader(isr);
         String nextLine = br.readLine();
-        System.out.println(nextLine);
         JSONObject jObject = new JSONObject(nextLine);
 
         User user = new User();
@@ -101,6 +103,87 @@ public class SocialController {
             lionBoardService.securityLogin(user);
         }
         return "redirect:/index";
+    }
+
+    @RequestMapping(value = "/twitter")
+    public String twitter_access(HttpSession session) throws Exception{
+
+        OAuthHeaderTwitter twitter_api = new OAuthHeaderTwitter();
+
+        // 트위터 앱 생성후 받은 토큰
+        twitter_api.setConsumer_key(environment.getProperty("twitter.app.id"));
+        twitter_api.setConsumer_secret(environment.getProperty("twitter.app.secret"));
+
+
+
+        // 인증 요청 토큰 생성
+        twitter_api.setRequestToken();
+
+        String requestToken = twitter_api.getRequestToken().getToken();
+        String requestTokenSecret = twitter_api.getRequestToken().getTokenSecret();
+
+        System.out.println("after redirecting, request Token is " + requestToken);
+
+        //Todo : store the requestToken and secret to redis cache.
+
+        String authenticationUrl = twitter_api.getRequestToken().getAuthenticationURL();
+
+
+        // 인증url 리다이렉트
+        return "redirect:"+authenticationUrl;
+
+    }
+
+    @RequestMapping(value = "/twitter_success")
+    public String twitter_success(HttpSession session, HttpServletRequest request) throws Exception {
+
+        // 트위터에서 받은 인증토큰
+        String oauthToken = request.getParameter("oauth_token"); // requestToken 토큰과 같은 값이여야 한다. (보안)
+        String oauthVerifier = request.getParameter("oauth_verifier"); // 트위터에서 인증요청하면서 생성한 토큰
+
+        //Todo : Get the requestToken and secret from redis cache.
+        String requestToken = (String) session.getAttribute("requestToken");
+        String requestTokenSecret = (String) session.getAttribute("requestTokenSecret");
+        String contact = null;
+
+        // 보안상 토큰이 일치하는 지 비교.
+        if (requestToken.equals(oauthToken)) {
+
+            OAuthHeaderTwitter twitter_api = new OAuthHeaderTwitter();
+            twitter_api.setConsumer_key("oi9uv7nIxPqcTTKu3WpOQeW4C");
+            twitter_api.setConsumer_secret("d26Hq4TMBo2uNOplbI5hbmKObXKB4BUgl335ODOYNfymawVkv3");
+
+
+            // 인증된 토큰 생성
+            twitter_api.setAccessToken(requestToken, requestTokenSecret, oauthVerifier);
+
+            // 인증된 토큰을 변수에 담는다.
+            String accessToken = twitter_api.getAccessToken().getToken();
+            String accessTokenSecret = twitter_api.getAccessToken().getTokenSecret();
+
+
+            User user = new User();
+            user.setIdentity(Long.toString(twitter_api.getTwitter().verifyCredentials().getId()));
+            user.setEmail(Long.toString(twitter_api.getTwitter().verifyCredentials().getId()));
+            user.setName(twitter_api.getTwitter().verifyCredentials().getName());
+            user.setProfileUrl(twitter_api.getTwitter().verifyCredentials().getOriginalProfileImageURL());
+            user.setRoles("ROLE_USER");
+            user.setIsOAuth("T");
+            user.setPassword("");
+
+            User existedUser = lionBoardService.existUserByIdentity(user.getIdentity());
+            if (existedUser == null) {
+                lionBoardService.addUser(user);
+                lionBoardService.securityLogin(user);
+            } else {
+                lionBoardService.securityLogin(user);
+            }
+
+            return "redirect:/index";
+        }else{
+            throw new InvalidUserException("twitter login fail.");
+        }
+
     }
 
     //TODO facebook,twitter,kakao,daum 각 provider 구별 방법
---
+++ lionboard-web/src/main/java/common/OAuthHeaderTwitter.java
@@ -0,0 +1,74 @@
+package common;
+
+/**
+ * Created by Lion.k on 16. 2. 9..
+ */
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+public class OAuthHeaderTwitter {
+
+    private String consumer_key;
+
+    public void setConsumer_key(String consumer_key) {
+        this.consumer_key = consumer_key;
+    }
+
+    private String consumer_secret;
+
+    public void setConsumer_secret(String consumer_secret) {
+        this.consumer_secret = consumer_secret;
+    }
+
+    private RequestToken requestToken = null;
+    private AccessToken accessToken = null;
+    private Twitter twitter = null;
+
+
+    public OAuthHeaderTwitter() {
+        twitter = new TwitterFactory().getInstance();
+    }
+    public Twitter getTwitter(){
+        return twitter;
+    }
+
+    // 인증 요청 토큰 생성
+    public void setRequestToken()  throws TwitterException{
+        twitter.setOAuthConsumer(this.consumer_key, this.consumer_secret);
+        requestToken = twitter.getOAuthRequestToken("http://127.0.0.1:8080/auth/twitter_success");
+
+    }
+    public RequestToken getRequestToken(){
+        return requestToken;
+    }
+
+
+    // 인증된 토큰 생성
+    public void setAccessToken(String request_token, String request_tokensecret, String oauth_verifier)	 {
+        try {
+            twitter.setOAuthConsumer(this.consumer_key, this.consumer_secret);
+            accessToken = twitter.getOAuthAccessToken(new RequestToken(request_token, request_tokensecret),
+                    oauth_verifier);
+        } catch (TwitterException te) {
+            System.out.println(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + te);
+        }
+
+        twitter.setOAuthAccessToken(accessToken);
+    }
+    public AccessToken getAccessToken(){
+        return accessToken;
+    }
+
+    // 재인증 처리
+    public void signIn(String access_token, String access_tokensecret) {
+
+        twitter.setOAuthConsumer(this.consumer_key, this.consumer_secret);
+
+        twitter.setOAuthAccessToken(new AccessToken(access_token, access_tokensecret));
+    }
+
+}
